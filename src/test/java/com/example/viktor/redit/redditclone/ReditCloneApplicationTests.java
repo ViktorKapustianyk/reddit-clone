ac714fac661ad9fc21c1df42dfbff3db261fa179
ViktorKapustianyk <vityakap@gmail.com>
2024-02-28 17:18:03 +0200

    Initial commit

---
+++ src/test/java/com/example/viktor/redit/redditclone/ReditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viktor.redit.redditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReditCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
